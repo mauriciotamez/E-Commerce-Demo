6c14bf140c80e39aafa280900260e8725f295faa
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-02-09 18:46:33 -0600

    Fixed a bug in our react-router structure that prevented us to reach / path when going into the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 
 import './App.css'
+import MainLayout from './components/MainLayout'
 import ProtectedRoutes from './components/ProtectedRoutes'
 import { Cart, Login, Shop, ShopDetail } from './pages'
 
@@ -8,17 +9,21 @@ function App () {
   return (
 
     <HashRouter>
+      
       <Routes>
-        <Route>
-
-          <Route path='/' element={<Login />} />
-
-          <Route element={<ProtectedRoutes />} />
-          <Route path='/shop' element={<Shop />} />
-          <Route path='/shop/:id' element={<ShopDetail />} />
-          <Route path='/cart' element={<Cart />} />
-
-        </Route>
+        
+        <Route path='/login' element={<Login />} />
+        
+        <Route path='/' element={<Navigate to="/shop" />} />
+          
+          <Route element={<ProtectedRoutes />} >
+            <Route element={<MainLayout/>} >
+                <Route path='/shop' element={<Shop />} />
+                <Route path='/shop/:id' element={<ShopDetail />} />
+                <Route path='/cart' element={<Cart />} />
+            </Route>
+          </Route>
+        
       </Routes>
     </HashRouter>
 
