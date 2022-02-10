09f8188d5f189ec572546d42da96f8246baf8c35
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-02-10 02:23:00 -0600

    write the component  itself, it was a boilerplate before, need to add more styling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getCartThunk } from '../redux/actions';
 
 const Cart = () => {
+  
+  const dispatch = useDispatch()
+  const cart = useSelector( state => state.cart)
+  console.log(cart);
+  
+  useEffect(() =>  dispatch(getCartThunk()), []);
   return (
-    <p> Cart </p>
-  )
+    <div>
+      <h1>cart</h1>
+     <ul>
+      {
+        cart.map(product => (
+         <li key={product.product?.id}> <Link to={`/shop/${product.product?.id}`} >{product.product.name}</Link></li>
+        ))
+      }
+      </ul>
+    </div>
+    )
 }
 
 export default Cart
