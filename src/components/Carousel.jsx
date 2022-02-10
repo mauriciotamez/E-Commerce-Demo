7aae7c880a94362e1396bc31f0a906f208b1f9eb
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-02-10 02:21:31 -0600

    refactored a variable name to be more precise and easy to understand

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from 'react-icons/ai'
 
@@ -8,7 +8,7 @@ const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
 
   const carouselImages = []
-  const carouselImagess = productDetail.images?.map(image => {
+  const getCarouselImages = productDetail.images?.map(image => {
     const url = image.url
     return carouselImages.push(url)
   })
