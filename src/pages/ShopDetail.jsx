e4d0721fa1b633c9d7811e6b3692b09f5007e3a8
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-02-17 00:41:48 -0600

    Idented some JSX

--- src/pages/ShopDetail.jsx
+++ src/pages/ShopDetail.jsx
@@ -12,50 +12,52 @@ const ShopDetail = () => {
   const [quantity, setQuantity] = useState(0)
 
   const productDetail = useSelector(state => state.productDetail)
-  console.log(productDetail)
+  
 
   useEffect(() => {
     dispatch(getProductDetailThunk(id))
   }, [dispatch, id])
 
+  // the function that triggers in our onClick, that adds that specific product via an action
   const addProduct = () => {
     const product = {
       product: id,
       quantity: quantity
     }
-    dispatch(addProductThunk((product)));
+    dispatch(addProductThunk((product)))
   }
 
   return (
-    <div className=' px-0 sm:pt-10 sm:px-10 md:pt-10 md:px-20  xl:pt-10 xl:px-44'>
-      
-      <div className='pt-40 px-0 lg:flex  '>
+    <div className=' px-0 sm:pt-10  md:pt-10 md:px-20  xl:pt-10 xl:px-44'>
+      <div className='pt-20 px-0 lg:flex  '>
         <div className='basis-1/2'> <Carousel /> </div>
-        <div className='basis-1/2 mx-20 '>
+        <div className='basis-1/2 mx-20  2xl:my-16'>
           <div className=''>
-            <h1 className=' text-center text-2xl font-formal mt-10
-               '
-            > {productDetail.name}
+            <h1 className=' text-center text-2xl font-formal mt-10'> {productDetail.name}
             </h1>
           </div>
-          <div>
-            <div className='flex flex-row'>
-              { quantity >= 1 &&
-              <button onClick={() => setQuantity(quantity - 1)}>-</button>
+          <div className='flex flex-col '>
+            <div className='flex flex-row justify-center py-5'>
+              {
+              quantity >= 1 &&
+                <button className='px-2 border-2 ' onClick={() => setQuantity(quantity - 1)}>
+                  -
+                </button>
               }
-              <div>{quantity}</div>
-              <button onClick={() => setQuantity(quantity + 1)} >+</button>
+              <div className='text-lg px-1'>{quantity}</div>
+              <button className='px-2 border-2 ' onClick={() => setQuantity(quantity + 1)}>
+                +
+              </button>
             </div>
-            <button onClick={addProduct}>Add to cart</button>
+            <button className='font-formal py-1 border-2  ' onClick={addProduct}>Add to cart</button>
           </div>
           <div>
-            <h2 className='text-center font-bold font-formal pt-5
-              '
+            <h2 className='text-center font-bold font-formal pt-5'
             >{productDetail.price}$
             </h2>
           </div>
           <div>
-            <p className='text-center text-sm px-4 pt-20 font-formal'>
+            <p className='text-center text-sm px-4 pt-10 font-formal'>
               {productDetail.description}
             </p>
           </div>
